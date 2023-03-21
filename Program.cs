aaa5f47ad9b8bab2d2e240e40ac54641c1509f3e
Claus Damgaard <jinxxed@outlook.com>
2023-03-21 21:05:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Indtast et tal mellem 1 og {int.MaxValue}.");
+var value = Console.ReadLine(); // Using var since user might enter anything.
+bool test = int.TryParse(value, out int result);
+if (test && result <= int.MaxValue && result > 0)
+{
+    for (int i = 1; i <= result; i++)
+    {
+        string output = "";
+        if (i % 3 == 0)
+        {
+            output += "Fizz";
+        }
+        if (i % 5 == 0)
+        {
+            output += "Buzz";
+        }
+        if (i % 7 == 0)
+        {
+            output += "Jazz";
+        }
+        if (output != "") Console.WriteLine(output);
+    }
+}
+else
+{
+    Console.WriteLine("Hvad fanden har du gang i makker?!");
+}
